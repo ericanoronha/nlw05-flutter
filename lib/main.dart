40a0bc1ea77468d4817a6a3e8604ca49cc23cd20
Erica Noronha <erica.noronha@live.com>
2021-04-23 19:36:42 -0300

    Aula #01 :rocket:

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:DevQuiz/core/app_widget.dart';
+import 'package:DevQuiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
